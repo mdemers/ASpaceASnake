b290382ca95b51c87ab171de151c48b85da7e71a
Mike Demers <mdemers@hagley.org>
2022-08-18 14:54:04 -0400

    add object scraper

--- get-object-access-restrict-notes.py
+++ get-object-access-restrict-notes.py
@@ -7,14 +7,11 @@ def get_restrictions(writer):
     for jsonFile in files:
           res_num = jsonFile.rstrip('.json')
           resource = json.load(open(jsonFile))
-          title = resource['title']
-          try:
-             eadid = resource['ead_id']
-          except:
-             eadid = "Not found"
+          title = resource['display_string']
+          ancestor = 'collection'
           publish = resource['publish']
           try:
-              restrictions = resource['restrictions']
+              restrictions = resource['restrictions_apply']
           except:
               restrictions = "Not found"
           for note in resource['notes']:
@@ -24,10 +21,10 @@ def get_restrictions(writer):
               except:
                 end_date = "NONE"
               for sub in note['subnotes']:
-                  writer.writerow({'ASpace_ID' : res_num, 'Title' : title, 'AccessionNumber' : eadid, 'Published?' : publish, 'Restricted?' : restrictions, 'Note_ID' : note['persistent_id'], 'Note_Type' : note['type'], 'End' : end_date, 'Note_Content': sub['content'] })
+                  writer.writerow({'AO_ID' : res_num, 'Title' : title, 'Published?' : publish, 'Restricted?' : restrictions, 'Note_ID' : note['persistent_id'], 'Note_Type' : note['type'], 'End' : end_date, 'Note_Content': sub['content'] })
 
 def write_notes_csv(csvName):
-    fieldnames = ['ASpace_ID', 'Title', 'AccessionNumber', 'Published?', 'Restricted?', 'Note_ID', 'Note_Type', 'End', 'Note_Content']
+    fieldnames = ['AO_ID', 'Title', 'Published?', 'Restricted?', 'Note_ID', 'Note_Type', 'End', 'Note_Content']
     with open(csvName, 'w', newline='') as outputFile:
         writer = csv.DictWriter(outputFile, fieldnames=fieldnames)
         writer.writeheader()
